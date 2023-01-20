0189ffd7742932f83c48d4d68454c5e80ae861f4
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-01-20 01:49:59 +0100

    Aggiunge card dettagli fumetto in show.blade

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -8,7 +8,18 @@
     <section class="container">
         <h1 class="text-center">Dettagli fumetto</h1>
         <a href="{{ route('comics.index') }}" class="btn btn-secondary my-3">Torna all'archivio</a>
-        <h2>{{ $comic->title }}</h2>
-        <p>{{ $comic->description }}</p>
+        <div class="card d-flex flex-row">
+            <img src="{{ $comic->thumb }}" class="img-fluid" alt="{{ $comic->title }}">
+            <div class="card-body">
+                <h3 class="card-title fw-bold">{{ $comic->title }}</h3>
+                <p class="card-text">{{ $comic->description }}</p>
+                <ul class="list-unstyled">
+                    <li><strong>Serie:</strong> {{ $comic->series }}</li>
+                    <li><strong>Genere:</strong> {{ $comic->type }}</li>
+                    <li><strong>Data di uscita:</strong> {{ $comic->sale_date }}</li>
+                    <li><strong>Prezzo:</strong> {{ $comic->price }}€</li>
+                </ul>
+            </div>
+          </div>
     </section>
 @endsection
