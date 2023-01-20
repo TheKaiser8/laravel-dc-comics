2ecafbf6bb4612722246d0dffd6e481dbb1ba57d
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-01-20 17:32:16 +0100

    Aggiunge bonus modale bootstrap (modalDelete)

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -29,17 +29,36 @@
                     <td>
                         <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-info">Mostra dettagli</a>
                         <a href="{{ route('comics.edit', $comic->id) }}" class="btn btn-warning">Modifica</a>
-                        <form action="{{ route('comics.destroy', $comic) }}" method="POST" class="mt-2">
-                            @csrf
-                            @method('DELETE')
-                            
-                            <button type="submit" class="btn btn-danger">Cancella</button>
-                        </form>
+
+                        <!-- Button modale (modalDelete) -->
+                        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modalDelete">Cancella</button>
                     </td>
                 </tr>
                 @endforeach
             </tbody>
         </table>
+
+        <!-- Modale (modalDelete) -->
+        <div class="modal fade" id="modalDelete" tabindex="-1" aria-labelledby="modalDeleteLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="modalDeleteLabel">Cancellazione elemento</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">Confermi di voler cancellare definitivamente questo elemento?</div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No, torna indietro</button>
+                        <form action="{{ route('comics.destroy', $comic) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            
+                            <button type="submit" class="btn btn-danger">Sì, cancella</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
         <a href="{{ route('comics.create') }}" class="btn btn-success">Aggiungi nuovo fumetto</a>
     </section>
 @endsection
