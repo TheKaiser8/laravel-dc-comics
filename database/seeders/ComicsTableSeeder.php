6942bc27d8af001ac0655d236e42ebb0971a2bb2
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-01-23 14:35:53 +0100

    Refactoring comics seeder e aggiorna consegna Commenta proprietà oggetto e utilizza metodo fill in comics seeder

--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -19,13 +19,14 @@ class ComicsTableSeeder extends Seeder
 
         foreach ($comics as $comic) {
             $new_comic = new Comic();
-            $new_comic->title = $comic['title'];
-            $new_comic->description = $comic['description'];
-            $new_comic->thumb = $comic['thumb'];
-            $new_comic->price = $comic['price'];
-            $new_comic->series = $comic['series'];
-            $new_comic->sale_date = $comic['sale_date'];
-            $new_comic->type = $comic['type'];
+            // $new_comic->title = $comic['title'];
+            // $new_comic->description = $comic['description'];
+            // $new_comic->thumb = $comic['thumb'];
+            // $new_comic->price = $comic['price'];
+            // $new_comic->series = $comic['series'];
+            // $new_comic->sale_date = $comic['sale_date'];
+            // $new_comic->type = $comic['type'];
+            $new_comic->fill($comic);
             $new_comic->save();
         }
     }
