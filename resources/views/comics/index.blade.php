3b7a035eaa8b42b526895d2c5621c6d0fedce0ff
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-01-27 09:44:07 +0100

    Fix modalDelete in tabella index Crea una modale per ogni singolo fumetto con id dinamico

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -27,38 +27,38 @@
                     <td>{{ $comic->sale_date }}</td>
                     <td>{{ $comic->price }}€</td>
                     <td>
-                        <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-info">Mostra dettagli</a>
+                        <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-info">Dettagli</a>
                         <a href="{{ route('comics.edit', $comic->id) }}" class="btn btn-warning">Modifica</a>
 
                         <!-- Button modale (modalDelete) -->
-                        <button type="button" class="btn btn-danger mt-1" data-bs-toggle="modal" data-bs-target="#modalDelete">Cancella</button>
+                        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modalDelete-{{ $comic->id }}">Cancella</button>
                     </td>
                 </tr>
+
+                <!-- Modale (modalDelete) -->
+                <div class="modal fade" id="modalDelete-{{ $comic->id }}" tabindex="-1" aria-labelledby="modalDeleteLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="modalDeleteLabel">Cancellazione elemento</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">Confermi di voler cancellare definitivamente il fumetto "<strong>{{ $comic->title }}</strong>"?</div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                                <form action="{{ route('comics.destroy', $comic) }}" method="POST" class="d-inline-block">
+                                    @csrf
+                                    @method('DELETE')
+                                    
+                                    <button type="submit" class="btn btn-danger">Sì, cancella</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 @endforeach
             </tbody>
         </table>
-
-        <!-- Modale (modalDelete) -->
-        <div class="modal fade" id="modalDelete" tabindex="-1" aria-labelledby="modalDeleteLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="modalDeleteLabel">Cancellazione elemento</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">Confermi di voler cancellare definitivamente questo elemento?</div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No, torna indietro</button>
-                        <form action="{{ route('comics.destroy', $comic) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            
-                            <button type="submit" class="btn btn-danger">Sì, cancella</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
         <a href="{{ route('comics.create') }}" class="btn btn-success">Aggiungi nuovo fumetto</a>
     </section>
 @endsection
